000d786602daa49d9ea72780d635a42e8f2127a8
tainguyen25403 <nttai254@gmail.com>
2025-06-03 15:52:09 +0700

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -40,6 +40,10 @@ export default function RootLayout({
           name="viewport"
           content="width=device-width, initial-scale=1, maximum-scale=1"
         />
+        <link
+          rel="icon"
+          href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🎓</text></svg>"
+        />
       </head>
       <body
         className={`${inter.variable} ${playfair.variable} ${raleway.variable} ${yeseva.variable} font-inter antialiased`}
